ea595018f49a83bcfc4982802704704b4d82a9f9
Seunghyun Lee <shlee0605@gmail.com>
2016-04-29 00:29:15 -0400

    added item price sum test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -29,18 +29,30 @@ public class Main {
             "explain analyze select replace_vowel_plpgsql(field1) from \"USERTABLE\";",
             "explain analyze select replace_vowel_c(field1) from \"USERTABLE\";"
         );
+
         experiment(
             Experiment.DBType.TPCC,
             "calc_tax",
             "explain analyze select i_price, calc_tax_plpgsql(i_price) from item;",
             "explain analyze select i_price, calc_tax_c(i_price) from item;"
         );
+
         experiment(
             Experiment.DBType.TPCC,
             "integer_manipulate",
             "explain analyze select integer_manipulate_plpgsql(i_im_id) from item;",
             "explain analyze select integer_manipulate_c(i_im_id) from item;"
         );
+        
+        experiment(
+            Experiment.DBType.TPCC,
+            "item_sales_sum",
+            "explain analyze select i_id, item_sales_sum_plpgsql(i_id) from item where i_id " +
+                    "in (100, 14232, 22352, 53421, 99322, 82312, 2214)",
+            "explain analyze select i_id, item_sales_sum_c(i_id) from item where i_id " +
+                    "in (100, 14232, 22352, 53421, 99322, 82312, 2214)"
+        );
+
 
     }
 }
