9cef50decfaf3f919a7446d3de38a837d0161813
Zheyuan Bu <buzheyuan@gmail.com>
2016-05-04 20:58:12 +0000

    fix buf

--- src/main/java/DBConnection.java
+++ src/main/java/DBConnection.java
@@ -7,14 +7,12 @@ import java.sql.*;
  */
 public class DBConnection {
 
-    public static final String DB_NAME_YCSB = "ycsb";
-    public static final String DB_NAME_TPCC = "tpcc";
+    public static final String DB_NAME = "postgres";
     public static final String DB_PORT = "57721";
     public static final String USER_NAME = "vagrant";
     public static final String USER_PASSWORD = "password";
 
-    Connection connYCSB;
-    Connection connTPCC;
+    Connection conn;
 
     public DBConnection() {
         connectToPeloton();
@@ -30,10 +28,7 @@ public class DBConnection {
         }
 
         try {
-            connYCSB = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:" + DB_PORT + "/" + DB_NAME_YCSB,
-                    USER_NAME, USER_PASSWORD);
-            connTPCC = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:" + DB_PORT + "/" + DB_NAME_TPCC,
-                    USER_NAME, USER_PASSWORD);
+            conn = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:" + DB_PORT + "/" + DB_NAME, USER_NAME, USER_PASSWORD);
 
         } catch (SQLException e) {
             System.out.println("Connection Failed! Check output console");
@@ -43,18 +38,8 @@ public class DBConnection {
     }
 
     // runQuery returns execution time of that query.
-    public double runQuery(String sql, Experiment.DBType type) {
-        Connection conn;
+    public double runQuery(String sql) {
         double executionTime = 0;
-
-        if (type == Experiment.DBType.YCSB) {
-            conn = connYCSB;
-        } else if (type == Experiment.DBType.TPCC) {
-            conn = connTPCC;
-        } else {
-            System.out.println("Wrong input type.");
-            return 0;
-        }
 
         try {
             Statement stmt = conn.createStatement();
@@ -74,16 +59,7 @@ public class DBConnection {
         return executionTime;
     }
 
-    public void runUpdate(String sql, Experiment.DBType type) {
-        Connection conn;
-        if (type == Experiment.DBType.YCSB) {
-            conn = connYCSB;
-        } else if (type == Experiment.DBType.TPCC) {
-            conn = connTPCC;
-        } else {
-            System.out.println("Wrong input type.");
-            return;
-        }
+    public void runUpdate(String sql) {
 
         try {
             Statement stmt = conn.createStatement();
@@ -97,8 +73,7 @@ public class DBConnection {
 
     public void closeConnection() {
         try {
-            connTPCC.close();
-            connYCSB.close();
+            conn.close();
         } catch(SQLException e) {
             System.out.println("Connection is not closed correctly.");
             e.printStackTrace();
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,60 +3,55 @@
  */
 public class Main {
 
-    private static void experiment(Experiment.DBType type, String funcName, String pSQL, String cSQL) {
+    private static void experiment(String funcName, String pSQL, String cSQL) {
         // initialize db connection
         DBConnection connection = new DBConnection();
         // create experiment instance
         Experiment experiment = new Experiment(connection);
         // run experiment
-        ExperimentResult result = experiment.runBasicExperiment(type, funcName, pSQL, cSQL);
+        ExperimentResult result = experiment.runBasicExperiment(funcName, pSQL, cSQL);
         result.printResult();
         connection.closeConnection();
     }
 
-    private static void experimentStoredProcedureOne(Experiment.DBType type) {
+    private static void experimentStoredProcedureOne() {
         DBConnection connection = new DBConnection();
         Experiment experiment = new Experiment(connection);
 
-        ExperimentResult result = experiment.runStoredProcedureExperimentOne(type);
+        ExperimentResult result = experiment.runStoredProcedureExperimentOne();
         result.printResult();
         connection.closeConnection();
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("------------- Peloton UDF Comparison Testing ------------");
         System.out.println("function name\t| c time \t | plpgsql time");
-
         experiment(
-            Experiment.DBType.YCSB,
             "concat_text",
             "explain analyze select concat_text_plpgsql(field1, field2) from \"USERTABLE\";",
             "explain analyze select concat_text_c(field1, field2) from \"USERTABLE\";"
         );
 
         experiment(
-            Experiment.DBType.YCSB,
             "replace_vowel",
             "explain analyze select replace_vowel_plpgsql(field1) from \"USERTABLE\";",
             "explain analyze select replace_vowel_c(field1) from \"USERTABLE\";"
         );
 
+
         experiment(
-            Experiment.DBType.TPCC,
             "calc_tax",
             "explain analyze select i_price, calc_tax_plpgsql(i_price) from item;",
             "explain analyze select i_price, calc_tax_c(i_price) from item;"
         );
 
         experiment(
-            Experiment.DBType.TPCC,
             "integer_manipulate",
             "explain analyze select integer_manipulate_plpgsql(i_im_id) from item;",
             "explain analyze select integer_manipulate_c(i_im_id) from item;"
         );
 
         experiment(
-                Experiment.DBType.TPCC,
                 "item_sales_sum",
                 "explain analyze select i_id, item_sales_sum_plpgsql(i_id) from item where i_id " +
                         "in (100, 14232, 22352, 53421, 99322, 82312, 2214)",
@@ -64,15 +59,12 @@ public class Main {
                         "in (100, 14232, 22352, 53421, 99322, 82312, 2214)"
         );
 
-
         // Stored Procedure Test
         System.out.println();
         System.out.println("------------- Stored Procedure Testing ------------");
         System.out.println("experiment name\t| sql time \t | stored proc time");
 
-        experimentStoredProcedureOne(
-            Experiment.DBType.TPCC
-        );
+        experimentStoredProcedureOne();
 
     }
 }
